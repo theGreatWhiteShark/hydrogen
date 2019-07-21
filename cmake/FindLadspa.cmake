c1615044d0ea77b70c1bbfa53b3ef7944d66cdcc
theGreatWhiteShark <thetruephil@googlemail.com>
2019-07-21 11:48:30 +0200

    finding LADSPA plugins using cmake

--- cmake/FindLadspa.cmake
+++ cmake/FindLadspa.cmake
@@ -8,35 +8,63 @@
 
 macro(FIND_LADSPA prefix header lib)
 
-    if(${prefix}_INCLUDE_DIR AND ${prefix}_LIBRARIES)
-        set(${prefix}_FIND_QUIETLY TRUE)
-    else()
-        MESSAGE(STATUS "Checking for module 'LADSPA'")
-        
-        set(CMAKE_PREFIX_PATH ${${prefix}_PATH} )
+  if(${prefix}_INCLUDE_DIR AND ${prefix}_LIBRARIES)
+    set(${prefix}_FIND_QUIETLY TRUE)
+  else()
+    MESSAGE(STATUS "Checking for module 'LADSPA'")
 
-        find_path(${prefix}_INCLUDE_DIR
-            NAMES ${header}
-            PATHS ${${prefix}_INCLUDE_DIRS} ${${prefix}_INCLUDEDIR}
+	if(APPLE)
+
+	  ## Check for the existence of LADSPA shared objects
+	  if(EXISTS "/usr/local/lib/ladspa")
+
+		SET(${prefix}_INCLUDE_DIR "/usr/local/lib/ladspa")
+
+		file(GLOB_RECURSE LADSPA_PLUGINS "/usr/local/lib/ladspa/*.so" LIST_DIRECTORIES false)
+
+        list(LENGTH LADSPA_PLUGINS PLUGINS_N)
+        if(PLUGINS_N GREATER 0)
+          STRING(REGEX REPLACE ":" "" LADSPA_PLUGINS "${LADSPA_PLUGINS}")
+          SET(${prefix}_LIBRARIES "${PLUGINS_N} plugins found" )
+          MESSAGE(STATUS "  ${PLUGINS_N} plugins found.")
+
+		endif()
+
+		SET(${prefix}_FOUND)
+
+	  include(FindPackageHandleStandardArgs)
+	  FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIR)
+	  mark_as_advanced(${prefix}_INCLUDE_DIR  ${prefix}_LIBRARIES)
+
+	  endif()
+	  
+	else()
+      
+      set(CMAKE_PREFIX_PATH ${${prefix}_PATH} )
+
+      find_path(${prefix}_INCLUDE_DIR
+        NAMES ${header}
+        PATHS ${${prefix}_INCLUDE_DIRS} ${${prefix}_INCLUDEDIR}
         )
-        
-        find_program( LISTPLUGINS NAMES listplugins )
-        
-        if( LISTPLUGINS )
-            execute_process( OUTPUT_FILE "${CMAKE_BINARY_DIR}/ladspa_listplugins" COMMAND ${LISTPLUGINS} )
-            file( STRINGS "${CMAKE_BINARY_DIR}/ladspa_listplugins" LADSPA_PLUGINS REGEX "ladspa/.*\\.so:" )
-            list( LENGTH LADSPA_PLUGINS PLUGINS_N )
-            if( PLUGINS_N GREATER 0 )
-                STRING(REGEX REPLACE ":" "" LADSPA_PLUGINS "${LADSPA_PLUGINS}")
-                SET(${prefix}_LIBRARIES "${PLUGINS_N} plugins found" )
-                MESSAGE(STATUS "  ${PLUGINS_N} plugins found.")
-            endif( )
-        endif()
+      
+      find_program( LISTPLUGINS NAMES listplugins )
+      
+      if( LISTPLUGINS )
+        execute_process( OUTPUT_FILE "${CMAKE_BINARY_DIR}/ladspa_listplugins" COMMAND ${LISTPLUGINS} )
+        file( STRINGS "${CMAKE_BINARY_DIR}/ladspa_listplugins" LADSPA_PLUGINS REGEX "ladspa/.*\\.so:" )
+        list( LENGTH LADSPA_PLUGINS PLUGINS_N )
+        if( PLUGINS_N GREATER 0 )
+          STRING(REGEX REPLACE ":" "" LADSPA_PLUGINS "${LADSPA_PLUGINS}")
+          SET(${prefix}_LIBRARIES "${PLUGINS_N} plugins found" )
+          MESSAGE(STATUS "  ${PLUGINS_N} plugins found.")
+        endif( )
+      endif()
 
-    endif()
+	  include(FindPackageHandleStandardArgs)
+	  FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${finprefix}_LIBRARIES ${prefix}_INCLUDE_DIR)
+	  mark_as_advanced(${prefix}_INCLUDE_DIR  ${prefix}_LIBRARIES)
 
-    include(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(${prefix} DEFAULT_MSG ${prefix}_LIBRARIES ${prefix}_INCLUDE_DIR)
-    mark_as_advanced(${prefix}_INCLUDE_DIR  ${prefix}_LIBRARIES)
+	endif()
+  endif()
 
 endmacro()
